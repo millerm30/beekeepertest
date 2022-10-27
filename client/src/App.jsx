1284f58347e9e14e50d3917e7b256c0099335e57
Michael Miller <millergm30@gmail.com>
2022-10-27 09:47:44 -0230

    added spinner loading for api calls

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { TiDelete } from 'react-icons/ti';
+import Header from './components/Header';
+import Spinner from './components/Loading';
 
 const App = () => {
   const [ users, setUsers ] = useState([]);
-  const [ firstName, setFirstName ] = useState("");
-  const [ lastName, setLastName ] = useState("");
   const [ newUser, setNewUser ] = useState(0);
+  const [ isLoading, setIsLoading ] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   useEffect(() => {
     axios
       .get("https://beekeepertest.herokuapp.com/users")
       .then((res) => {
         setUsers(res.data);
+        setIsLoading(false);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, [newUser] );
+  }, [newUser]);
 
   const addNewUser = (e) => {
     e.preventDefault();
+    setIsLoading(true);
     axios
       .post("https://beekeepertest.herokuapp.com/users", {
         first_name: firstName,
@@ -29,22 +34,30 @@ const App = () => {
       .then((res) => {
         setNewUser(newUser + 1);
         console.log(firstName, lastName);
+        setFirstName("");
+        setLastName("");
+      })
+      .catch((err) => {
+        console.log(err);
       });
-    setFirstName("");
-    setLastName("");
   };
 
   const deleteUser = (id) => {
+    setIsLoading(true);
     axios
       .delete(`https://beekeepertest.herokuapp.com/users/${id}`)
       .then((res) => {
         setNewUser(newUser - 1);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
       });
   };
 
   return (
     <div className="w-3/4 ml-auto mr-auto mt-4 md:w-1/2 lg:w-1/2">
-      <h1 className="text-center text-2xl font-bold">User Database Testing</h1>
+      <Header />
       <form
         onSubmit={addNewUser}
         className="flex flex-col bg-white p-6 rounded-lg border-2 shadow-md my-6"
@@ -80,48 +93,48 @@ const App = () => {
           Add User
         </button>
       </form>
-      {!users.length <= 0 ? (
-      <table className="w-full mt-4">
-        <thead className="border-b-2 border-blue-500">
-          <tr>
-            <th>
-              ID
-            </th>
-            <th>
-              First Name
-            </th>
-            <th>
-              Last Name
-            </th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr
-              key={user.user_id}
-              className="text-center"
-            >
-              <td>{user.user_id}</td>
-              <td>{user.first_name}</td>
-              <td>{user.last_name}</td>
-              <td className='flex justify-center'>
-                <TiDelete 
-                  onClick={() => deleteUser(user.user_id)}
-                  className="text-red-500 cursor-pointer hover:text-red-700 text-xl"
-                >
-                  Delete 
-                </TiDelete>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+
+      {!isLoading ? (
+        <>
+          {!users.length <= 0 ? (
+            <table className="w-full mt-4">
+              <thead className="border-b-2 border-blue-500">
+                <tr>
+                  <th>ID</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((user) => (
+                  <tr key={user.user_id} className="text-center">
+                    <td>{user.user_id}</td>
+                    <td>{user.first_name}</td>
+                    <td>{user.last_name}</td>
+                    <td className="flex justify-center">
+                      <TiDelete
+                        onClick={() => deleteUser(user.user_id)}
+                        className="text-red-500 cursor-pointer hover:text-red-700 text-xl"
+                      >
+                        Delete
+                      </TiDelete>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <h1 className="text-center text-2xl font-bold mt-4">
+              No Users Found!
+            </h1>
+          )}
+        </>
       ) : (
-        <h1 className="text-center text-2xl font-bold mt-4">No Users Found!</h1>
+        <Spinner />
       )}
     </div>
   );
-}
+};
 
 export default App
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-center text-2xl font-bold">User Database Testing</h1>
+    </div>
+  );
+};
+
+export default Header
